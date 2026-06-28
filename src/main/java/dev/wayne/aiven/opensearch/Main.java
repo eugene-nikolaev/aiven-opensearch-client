879ba19a611bd8171ae623f62d41aea794858203
Geni Nikolaev <eug.nikolaev@gmail.com>
2026-06-28 23:01:53 +0300

    Handle CLI errors gracefully

--- src/main/java/dev/wayne/aiven/opensearch/Main.java
+++ src/main/java/dev/wayne/aiven/opensearch/Main.java
@@ -3,12 +3,20 @@ package dev.wayne.aiven.opensearch;
 public final class Main {
     private Main() {}
 
-    public static void main(String[] args) throws Exception {
-        OpenSearchConfig config = OpenSearchConfig.fromEnvironment();
+    public static void main(String[] args) {
+        try {
+            OpenSearchConfig config = OpenSearchConfig.fromEnvironment();
 
-        try (OpenSearchConnection connection = OpenSearchClientFactory.create(config)) {
-            var info = connection.client().info();
-            System.out.println(info.version().number());
+            try (OpenSearchConnection connection = OpenSearchClientFactory.create(config)) {
+                var info = connection.client().info();
+                System.out.println(info.version().number());
+            }
+        } catch (IllegalArgumentException exception) {
+            System.err.println("Configuration error: " + exception.getMessage());
+            System.exit(2);
+        } catch (Exception exception) {
+            System.err.println("Failed to connect to OpenSearch: " + exception.getMessage());
+            System.exit(1);
         }
     }
 }
